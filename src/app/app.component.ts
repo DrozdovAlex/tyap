f4beb779e0065d272687d47922e1465f28d4c789
adrozdov <alexandr.drozdov@RealPage.com>
2019-12-11 03:25:23 +0700

    add changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,35 +15,36 @@ export class AppComponent {
   endChain = '';
   chain = '';
   answer = '';
+  lengthRV = 0;
 
   getExpression() {
-    this.chain = '';
     this.dop = this.multiplicity - (this.initChain.length + this.endChain.length) % this.multiplicity;
+    this.chain = '';
     if (this.initChain !== '') {
-      this.chain = '(' + this.initChain + ')';
-    }
+        this.chain = '(' + this.initChain + ')';
+      }
 
     this.chain += '(';
     for (let i = 0; i < this.dop; i++) {
-      this.chain += '(';
-      for (let j = 0; j < this.alphabet.length; j++) {
-        if (j !== 0) {
-          this.chain += '+' + this.alphabet[j];
-        } else {
-          this.chain += this.alphabet[j];
+        this.chain += '(';
+        for (let j = 0; j < this.alphabet.length; j++) {
+          if (j !== 0) {
+            this.chain += '+' + this.alphabet[j];
+          } else {
+            this.chain += this.alphabet[j];
+          }
         }
+        this.chain += ')';
       }
-      this.chain += ')';
-    }
-    this.chain += ')';
+    this.chain += ')*';
 
     if (this.endChain !== '') {
-      this.chain += '*(' + this.endChain + ')';
-    }
+        this.chain += '(' + this.endChain + ')';
+      }
 
     if (this.multiplicity === 1) {
-      this.chain += '+' + this.alphabet;
-    }
+        this.chain += '+' + this.alphabet;
+      }
   }
 
   getChains() {
@@ -52,6 +53,30 @@ export class AppComponent {
 
     for (let i = 0; i < this.alphabet.length; i++) {
       alphabetMassive.push(this.alphabet[i]);
+    }
+
+    let lala;
+    for (let i = 0; i < this.initChain.length + 3; i++) {
+      for (let j = 0; j < this.endChain.length + 3; j++) {
+        if (this.initChain.length > this.endChain.length) {
+           if (this.initChain[this.initChain.length - this.endChain.length + i] === this.endChain[j]) {
+            lala = this.initChain.substring(0, this.initChain.length - 1 - i);
+            console.log(lala);
+            if (this.multiplicity === (lala + this.endChain).length) {
+              this.answer += lala + this.endChain + ' ';
+            }
+          }
+        } else {
+          if (this.initChain[i] === this.endChain[j]) {
+            lala = this.initChain.substring(0, this.initChain.length - i);
+            console.log(lala);
+            if (this.multiplicity === (lala + this.endChain).length) {
+              this.answer += lala + this.endChain + ' ';
+            }
+          }
+        }
+
+      }
     }
 
     if (this.multiplicity === (this.initChain.length + this.endChain.length) || (this.initChain.length + this.endChain.length) % this.multiplicity === 0) {
@@ -78,18 +103,110 @@ export class AppComponent {
       } while (this.maxLength >= (this.dop + this.initChain.length + this.endChain.length));
     }
   }
+
+  setChains() {
+    let flag1 = true;
+    let flag2 = false;
+    let flag3 = false;
+    this.initChain = '';
+    this.endChain = '';
+    this.multiplicity = 0;
+    this.lengthRV = 0;
+    let index = 0;
+    while (flag1 === true) {
+      if (this.chain[index] !== '(' && this.chain[index] !== ')') {
+        this.initChain += this.chain[index];
+
+      }
+
+      if (this.chain[index] === ')') {
+        flag1 = false;
+        flag2 = true;
+      }
+      index++;
+      console.log(this.initChain);
+    }
+
+    while (flag2 === true) {
+      if (this.chain[index] !== '(' && this.chain[index] !== ')' && this.chain[index] !== '+' && this.chain[index] !== '*') {
+        this.alphabet += this.chain[index];
+      }
+
+      if (this.chain[index] === ')') {
+        this.lengthRV++;
+      }
+
+      if (this.chain[index] === ')' && this.chain[index + 1] === '*') {
+        this.alphabet = this.unique(this.alphabet).toString().replace(/,/g, '');
+        this.lengthRV--;
+        flag2 = false;
+        flag3 = true;
+      }
+      index++;
+    }
+
+    while (flag3 === true) {
+      if (this.chain[index] !== '(' && this.chain[index] !== ')' && this.chain[index] !== '+' && this.chain[index] !== '*') {
+        this.endChain += this.chain[index];
+      }
+
+      if (this.chain[index] === ')') {
+        flag3 = false;
+      }
+      index++;
+    }
+    if (this.lengthRV === (0)) {
+      let lala;
+      for (let i = 0; i < this.initChain.length + 3; i++) {
+        for (let j = 0; j < this.endChain.length + 3; j++) {
+          if (this.initChain.length > this.endChain.length) {
+            if (this.initChain[this.initChain.length - this.endChain.length + i] === this.endChain[j]) {
+              lala = this.initChain.substring(0, this.initChain.length - 1 - i);
+              console.log(lala);
+              if (this.multiplicity === (lala + this.endChain).length) {
+                this.answer += lala + this.endChain + ' ';
+              }
+            }
+          } else {
+            if (this.initChain[i] === this.endChain[j]) {
+              lala = this.initChain.substring(0, this.initChain.length - i);
+              console.log(lala);
+              if (this.multiplicity === (lala + this.endChain).length) {
+                this.answer += lala + this.endChain + ' ';
+              }
+            }
+          }
+
+        }
+      }
+    }
+    this.multiplicity = this.initChain.length + this.lengthRV + this.endChain.length;
+    this.getExpression();
+    this.getChains();
+  }
+
+  unique(arr) {
+    const result = [];
+
+    for (const str of arr) {
+      if (!result.includes(str)) {
+        result.push(str);
+      }
+    }
+
+    return result;
+  }
 }
 
 function PermutationsWithRepetition(src, len) {
 
   let K;
   let k;
-  let N;
   let n;
   let out;
   let stack;
   K = len - 1,  k = 0,
-    N = src.length, n = 0,
+    n = 0,
     out = [],
     stack = [];
 
@@ -114,34 +231,7 @@ function PermutationsWithRepetition(src, len) {
     return false;
   }
 
-  function rewind() { k = 0; n = 0; out = []; stack = []; }
-
-  function each(cb) {
-    rewind();
-    let v;
-    while (v === next()) { if (cb(v) === false) { return; } }
-  }
-
   return {
     next,
-    each,
-    rewind
   };
 }
-
-/* пример использования */
-
-
- // [1, 1, 1]
-// perms.next(); // [1, 1, 2]
-// // ...
-// perms.next(); // [5, 5, 4]
-// perms.next(); // [5, 5, 5]
-// perms.next(); // false
-//
-// perms.rewind();
-// perms.next(); // [1, 1, 1]
-// ...
-
-
-// perms.each(function(v) { console.log(v); });
